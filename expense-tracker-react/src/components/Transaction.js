3d77751548ae051902536dc1ec0c4346e59b4e9d
vishalbhatd123456 <vishaldbhat123456@gmail.com>
2023-01-01 14:34:09 +0530

    Add and Delete transaction func

--- expense-tracker-react/src/components/Transaction.js
+++ expense-tracker-react/src/components/Transaction.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({transaction}) => {
+
+  const {deleteTransaction} = useContext(GlobalContext);
     const sign = transaction.amount<0?'-':'+';
   return (
     <li className = {transaction.amount<0?'minus':'plus'}>
                 {transaction.text}
-                <span>{sign}${Math.abs(transaction.amount)}<button className="delete-btn">x</button></span>
+                <span>
+                  {sign}${Math.abs(transaction.amount)}<button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button></span>
      </li>
   )
 }
--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -21,9 +21,27 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    //Actions that make call to reducer
+
+    function deleteTransaction(id){
+        dispatch({
+            type:'DELETE_TRANSACTION',
+            payload:id
+        });
+    }
+    function addTransaction(transaction){
+        dispatch({
+            type:'ADD_TRANSACTION',
+            payload:transaction
+        });
+    }
+
     return (
+        //access the transaction functionalities : ADD, DELETE
         <GlobalContext.Provider value={{
-            transactions: state.transactions
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
